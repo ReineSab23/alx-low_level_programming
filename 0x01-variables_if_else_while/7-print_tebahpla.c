e4e14eacf49cca381d2a375758b389b855f5a3ed
Reine Sabrina Ciella <reinesabrina2004@gmail.com>
2023-08-11 13:17:50 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * input - Entry point
+ *
+ * Description: print a-z in reverse
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+char ch = 'z';
+while (ch >= 'a')
+{
+putchar(ch);
+ch--;
+}
+putchar ('');
+return (0);
+}
