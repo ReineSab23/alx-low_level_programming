bf170314811e06b453e21f86c107f69c038fdac4
Reine Sabrina Ciella <reinesabrina2004@gmail.com>
2023-08-14 05:54:29 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry level
  *
  * Description: A program that prints a line to the standard error
- * Return: 1 (success)
+ *Return: 1 (error)
 */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(1, quo, 59);
 return (1);
 }
