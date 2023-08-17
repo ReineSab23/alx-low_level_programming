f02e92fe744580e0ebc19a1ff81934ec09e7dc49
Reine Sabrina Ciella <reinesabrina2004@gmail.com>
2023-08-17 11:02:09 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,9 @@
 void print_numbers(void)
 {
 int num = 0;
-do {
-_putchar(num + 48);
-num++;
-} while (num >= 0 && num <= 9)
-_putchar("\n");
+for (num = 0; num <= 9; num++)
+{
+_putchar(num + 0);
+}
+putchar("\n");
 }
