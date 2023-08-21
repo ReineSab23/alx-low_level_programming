d11113979342bbde7b777beee1ffea8244b198f0
Reine Sabrina Ciella <reinesabrina2004@gmail.com>
2023-08-21 09:45:36 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,12 +13,12 @@ void rev_string(char *s)
 int l, i;
 char temp;
 
-for (l = 0; s[i] != '\0'; ++1)
+for (l = 0; s[i] != '\0'; ++l)
 
 for (i = 0; i < l / 2; i++)
 {
 temp = s[i];
-s[i] = s[l - 1 - i];
-s[l - 1 - i] - temp;
+s[i] = s[l - l - i];
+s[l - l - i] - temp;
 }
 }
