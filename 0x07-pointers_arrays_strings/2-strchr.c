f48cd1cc08feeb119ad555e9c7d393c337392a6f
Reine Sabrina Ciella <reinesabrina2004@gmail.com>
2023-08-28 09:17:19 -0700

    a

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * *_strchr -  a function that locates a character in a string
+ *
+ * @s: pointer to the first occurence
+ * @c: input character
+ *
+ * Return: returns s
+*/
+char *_strchr(char *s, char c)
+{
+char i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+return (&s[i]);
+}
+}
+}
+
