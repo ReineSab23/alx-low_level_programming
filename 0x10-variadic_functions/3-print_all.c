4f117a44f0d916b9197ac0cb3c470036c06b7713
Reine Sabrina Ciella <reinesabrina2004@gmail.com>
2023-09-14 08:34:28 -0700

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -39,4 +39,39 @@ char *str - va_arg(ap, char *);
 switch ((int)(str))
 	case 1:
 	str = "(nil)";
-printf("%
+printf("%s%s", separator, str);
+}
+/**
+ * print_all - prints anything
+ * @format: the format string
+*/
+void print_all(const char * const format, ...)
+{
+int i = 0, j;
+char *separator = "";
+va_list ap;
+token_t tokens[] = {
+	{"c", format_char},
+	{"i", format_int},
+	{"f", format_float},
+	{"s", format_string},
+	{NULL , NULL}
+};
+va_start(ap, format);
+while (format && format[i])
+{
+	j = 0;
+	while (tokens[j].token)
+	{
+		if (format[i] == tokens[j].token[0])
+		{
+			tokens[j].f(separator, ap);
+			separator = ", ";
+		}
+		j++;
+	}
+	i++;
+}
+printf("\n");
+va_end(ap);
+}
